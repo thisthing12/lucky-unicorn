26092899a09098865c35c887d792e04d8ef4a321
thisthing12 <98915477+thisthing12@users.noreply.github.com>
2022-04-12 14:58:26 +1200

    last one

--- random v1.py
+++ random v1.py
@@ -2,7 +2,8 @@ def start_game(money_for_game):
     money_to_return = -1
     while money_for_game >= 1:
         print(f"You currently have ${money_for_game} in the game")
-        quit_chance = input("Do you want to spend $1 to start the game?(Y/N): ").upper()
+        quit_chance = input("Do you want to spend $1 to start the game?(Y/N): "
+                            "").upper()
         if quit_chance == 'Y':
             money_for_game -= 1
             money_for_game = chance_part(money_for_game)
